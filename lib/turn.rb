2e078de6922573776b6451420897d438178e31a1
Barbara Bontempo <barbarabontempo94@gmail.com>
2020-02-28 21:09:55 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,3 +12,12 @@ end
 puts "Welcome to Tic Tac Toe"
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 display_board(board)
+
+def input_to_index(user_input)
+  new_user_input = user_input.to_i
+  new_user_input -= 1
+end
+
+def move(board, index, character = "X")
+  board[index] = character
+end
