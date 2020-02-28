8982593de457dbec546efc11b292d943bf31e638
Barbara Bontempo <barbarabontempo94@gmail.com>
2020-02-28 21:13:58 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,11 +13,36 @@ puts "Welcome to Tic Tac Toe"
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 display_board(board)
 
-def input_to_index(user_input)
-  new_user_input = user_input.to_i
-  new_user_input -= 1
+#this converts the user input to an integer
+def input_to_index(user_input) #this method takes one argument (the users input)
+  new_user_input = user_input.to_i #this specific .to converts the string into an intergeer
+  new_user_input -= 1 #this subtracts it by 1 since arrays start with 0
 end
 
-def move(board, index, character = "X")
-  board[index] = character
+def move(board, index, character = "X") #this method takes on 3 arguments
+  board[index] = character #this will take the board array and place the character X onto the index
+end
+
+def valid_move?(board, index)
+  def position_taken?(array, ind)
+    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+      return false
+    else
+      return true
+    end
+  end
+
+  def available_on_board?(num)
+  if num.between?(0, 8) == true
+    return true
+  else
+    return false
+  end
+end
+
+  if (position_taken?(board, index)) == false && (available_on_board?(index) == true)
+    return true
+  else
+    return false
+  end
 end
